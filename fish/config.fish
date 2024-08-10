e9a2f4dfe3e3cc9a8307c5273706b3841098f55e
Stepan Dorohin <stepandorohin2@gmail.com>
2024-08-10 07:45:26 +0300

    feat: path for local utils

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
 set PATH /opt/homebrew/sbin $PATH
 set PATH /opt/homebrew/bin $PATH
+set PATH $PATH $HOME/.local/bin
+
+set -gx GPG_TTY (tty)
 
 alias ll='exa -alF'
 alias la='exa -a'
 alias l='exa -lF'
 alias tree='exa --tree -I ".venv"'
+alias zet='cd ~/Notes'
 
 set -U fish_greeting
