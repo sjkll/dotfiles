db4bb6fc9efc83db02301fdc94f1088e0dc2fad9
Stepan Dorohin <stepandorohin2@gmail.com>
2024-08-17 14:12:04 +0300

    feat(fish): new functions

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,11 @@ set PATH $PATH $HOME/.local/bin
 
 set -gx GPG_TTY (tty)
 
+alias ls='exa'
 alias ll='exa -alF'
 alias la='exa -a'
 alias l='exa -lF'
-alias tree='exa --tree -I ".venv"'
+alias tree='exa --tree -I ".venv|__pycache__"'
 alias zet='cd ~/Notes'
 
 set -U fish_greeting
