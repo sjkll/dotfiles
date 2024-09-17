5868ae1b03523e90733a0d759079554f79b88cd4
Stepan Dorohin <stepandorohin2@gmail.com>
2024-09-17 17:33:47 +0300

    feat(nvim): revert autocomplete

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -38,7 +38,7 @@ local lsp_settings = {
     directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
     semanticTokens = true,
   }, -- go
-  tsserver = {
+  ts_ls = {
     completions = {
       completeFunctionCalls = true,
     },
@@ -90,11 +90,11 @@ return {
     -- This event will simply after starting mason and mason.lspconfig
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
-      -- "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
       local lspconfig = require("lspconfig")
-      -- local cmp = require("cmp_nvim_lsp")
+      local cmp = require("cmp_nvim_lsp")
 
       lspconfig.util.on_setup = lspconfig.util.add_hook_after(lspconfig.util.on_setup, function(config)
         config.capabilities = vim.tbl_deep_extend("force", config.capabilities, cmp.default_capabilities())
